1673b7b81789c1f1fb161bf589eefc10e9678490
nuraxq <niklas.bueschelmann@web.de>
2025-07-19 18:31:42 +0200

    Task Two moved

--- main.cpp
+++ main.cpp
@@ -5,11 +5,5 @@
 
 int main()
 {
-    std::vector<int> v = {1,2,3};
-    int& wichtig = v[2];
-    std::cout << wichtig << std::endl;
-    wichtig++;
-    std::cout << wichtig << std::endl;
-    v.push_back(10);
-    std::cout << wichtig;
+    
 }
